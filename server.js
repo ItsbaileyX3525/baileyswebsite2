8a06519442107be8bbb48186b5a832050ff6fe36
bailey <baileyleah542@gmail.com>
2026-08-17 04:02:21 +0100

    uhh tailwind

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config();
 
-// Root route handler
+const app = express();
+
+const IS_DEV = (process.env.IS_DEV === "true");
+
+const PORT = IS_DEV ? 3000 : 443;
+
+app.use(express.static(path.join(__dirname, 'src')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Start listening for requests
 app.listen(PORT, () => {
+  console.log(IS_DEV);
   console.log(`Server is running at http://localhost:${PORT}`);
 });
